c21f7abc85807208b1ba7d5bf9c6dbacdeec249d
Shan-N <shantanu.neve2000@gmail.com>
2025-01-12 10:25:45 +0530

    compressed vid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,15 @@ import { Red_Rose } from 'next/font/google';
 const anton = Anton({
   subsets: ["latin"],
   weight: "400",
-})
+});
 const redRose = Red_Rose({
   subsets: ["latin"],
-})
+});
+
+const cardContent = [
+  {title:'', src:''},
+  {}
+]
 export default function Landing (){
   const sectionRef = useRef<HTMLElement>(null);
   const invFourRef = useRef<HTMLDivElement>(null);
@@ -24,7 +29,7 @@ export default function Landing (){
     <>  
     <Navbar />
     <div>
-      <video src='/testing.mp4' className="w-screen h-screen object-cover m-0 p-0" autoPlay loop muted/>
+      <video src='/testing_compressed.mp4' className="w-screen h-screen object-cover m-0 p-0" autoPlay loop muted/>
       <div className='bg-orange-600 py-5'></div>
       <div className='flex flex-col text-white'>
         <AnimatePresence>
@@ -48,11 +53,22 @@ export default function Landing (){
         className='max-w-auto flex items-start px-2 '>
           <Image src={invFour} alt='Invictus 4.0' className='size-64 md:size-96 object-cover relative z-30 -mt-10' />
         </motion.div>
-        <div className='bg-[#252525] pb-10 -mt-24 md:-mt-36 grid grid-rows-2 md:grid-cols-2 md:py-28'>
+        <div className='bg-[#252525] py-10 -mt-24 md:-mt-36 grid grid-rows-2 md:grid-cols-2 md:pt-28 md:pb-6'>
           <span className=''></span>
           <div className={twMerge(redRose.className,'flex flex-col px-2')}>
           <h2 className={twMerge(redRose.className,'text-lg md:text-2xl tracking-wide md:tracking-wider')}>At Team Solarium, we are fueled by a shared vision of promoting renewable energy and driving sustainable innovation.</h2>
           <a href='/about' className='flex items-end justify-end px-4 py-4 text-lg md:text-2xl'><span className={twMerge('border-b-4 border-orange-600',redRose.className)}>More about us</span></a>
+          </div>
+        </div>
+        <div>
+          <div className='px-4 py-6'>
+            <a href='/legacy' className=''><span className={twMerge(redRose.className,'text-2xl font-extrabold border-b-4 border-orange-600 pb-1')}>Legacy</span></a>
+          </div>
+          <div className='grid grid-rows-2 md:grid-cols-2'>
+            <p className='px-4 font-medium text-lg py-2'>Vehicle Description</p>
+            <div>
+              
+            </div>
           </div>
         </div>
       </div>
