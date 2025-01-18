2306a1f84d34aa87b16d5f885ac9a74ced213a16
Shan-N <shantanu.neve2000@gmail.com>
2025-01-18 18:55:12 +0530

    sequences

--- src/components/ui/PartnerGrid.tsx
+++ src/components/ui/PartnerGrid.tsx
@@ -23,34 +23,36 @@ import Image from 'next/image';
 import { twMerge } from 'tailwind-merge';
 
 const partnerLinks = [
-    {label:'Waaree', href: waareeLogo},
-    {label:'SolidWorks', href: solidWorksLogo},
-    {label:'Ansys', href: ansysLogo},
-    {label:'Molex', href: molexLogo},
-    {label:'PCB Market', href: pcbPowerLogo},
-    {label:'About Energy', href: aboutEnergyLogo},
-    {label:'Igus', href: igusLogo},
-    {label:'Nikol', href: nikolLogo},
-    {label:'Sunpower', href: sunPowerLogo},
-    {label:'Microtech', href: microTechLogo},
-    {label:'NIE', href: nieLogo},
-    {label:'Gripking', href: gripKingLogo},
-    {label:'Orient Tools', href: orientToolsLogo},
-    {label:'WE', href: weLogo},
-    {label:'Altium', href: altiumLogo},
-    {label:'SR Composites', href: srLogo},
-    {label:'Gurit', href: guritLogo},
-    {label:'Harnawa', href: harnawaLogo},
-    {label:'JR Sensors', href: jrLogo}
+    {label:'Waaree', href: waareeLogo,link:"https://waaree.com/"},
+    {label:'SolidWorks', href: solidWorksLogo,link:"https://www.solidworks.com/"},
+    {label:'Ansys', href: ansysLogo,link:"https://www.ansys.com/en-in"},
+    {label:'About Energy', href: aboutEnergyLogo,link:"https://www.aboutenergy.io/"},
+    {label:'PCB Market', href: pcbPowerLogo,link:"https://www.pcbpower.com/"},
+    {label:'Molex', href: molexLogo,link:"https://www.molex.com/en-us/home"},
+    {label:'Igus', href: igusLogo,link:"https://www.igus.in/"},
+    {label:'Nikol', href: nikolLogo,link:"https://www.linkedin.com/company/nikolindia/"},
+    {label:'Sunpower', href: sunPowerLogo,link:"https://us.sunpower.com/"},
+    {label:'Microtech', href: microTechLogo,link:""},
+    {label:'NIE', href: nieLogo,link:""},
+    {label:'Gripking', href: gripKingLogo,link:"https://gripkingtapes.com/"},
+    {label:'Orient Tools', href: orientToolsLogo,link:"http://orienttools.co.in/"},
+    {label:'WE', href: weLogo,link:"https://www.we-online.com/en"},
+    {label:'Altium', href: altiumLogo,link:"https://www.altium.com/"},
+    {label:'SR Composites', href: srLogo,link:"https://www.linkedin.com/company/srcomposites/"},
+    {label:'Gurit', href: guritLogo,link:"https://www.gurit.com/"},
+    {label:'Harnawa', href: harnawaLogo,link:"https://www.harnawa.in/"},
+    {label:'JR Sensors', href: jrLogo,link:"https://jrsensors.in/"}
   ]
 
 export const PartnerGrid = () => {
     return (
         <div className='grid grid-cols-4 grid-rows-4 gap-4 py-4'>
             {partnerLinks.map(link => (
-                <PartnerCard key={link.label}>
+                <a href={link.link} target='__blank' key={link.label}>
+                <PartnerCard>
                     <Image className={twMerge('size-20 md:size-32 object-contain')} src={link.href} alt={`${link.label}-Icon`} />
                 </PartnerCard>
+                </a>
             ))}
         </div>
     );
